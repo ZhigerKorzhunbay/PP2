a41c338c7e0c696460f291b05f54f5d9d5b52efa
Zhiger <zhigerkrzhunbay@gmail.com>
2026-01-24 18:59:48 +0600

    Add Python Strings

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,20 @@
+#Example 1
+b = "Hello, World!"
+print(b[-5:-2])
+
+#Example 2
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+#Example 3
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#Example 4
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+#Example 5
+txt = "We are the so-called \"Vikings\" from the north."
