4ca7a965e72627aaf01bac7d3bc01f5bb83f226c
Zhiger <zhigerkrzhunbay@gmail.com>
2026-01-24 18:45:43 +0600

    Add python-data-types

---
+++ Practice1/python-basics/data-types.py
@@ -0,0 +1,26 @@
+x = 5
+print(type(x))
+
+x = str("Hello World")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = frozenset(("apple", "banana", "cherry"))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = memoryview(bytes(5))
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+x = {"name" : "John", "age" : 36}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
