4115740fd12f1322f665a292c535f90bdeb4abc6
Zhiger <zhigerkrzhunbay@gmail.com>
2026-02-02 09:36:22 +0600

    Added more got-loop examples

--- Practice2/loops/for_loop.py
+++ Practice2/loops/for_loop.py
@@ -4,3 +4,27 @@ for x in fruits:
 #example2
 for x in "banana":
   print(x)
+#example3
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+#example4
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+#example5
+for x in range(2, 30, 3):
+  print(x)
+#example6
+for x in range(6):
+  print(x)
+#example7
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
