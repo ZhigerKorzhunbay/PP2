54361befc50cf69344b5c8b1b6fafd7724b3bc25
Zhiger <zhigerkrzhunbay@gmail.com>
2026-02-02 09:33:53 +0600

    Added for-break examples

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+#example2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
