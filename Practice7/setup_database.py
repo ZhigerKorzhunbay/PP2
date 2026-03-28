d8e273aa21dc0d9332c8ae9cbe27e913b219b7b5
Zhiger <zhigerkrzhunbay@gmail.com>
2026-03-28 16:13:07 +0600

    Modified setup_database

--- Practice7/setup_database.py
+++ Practice7/setup_database.py
@@ -1,21 +1,18 @@
 import psycopg2
 
-# Параметры подключения
 DB_CONFIG = {
     'host': 'localhost',
     'port': 5432,
     'user': 'postgres',
-    'password': '1234',  # Замени на свой пароль
-    'database': 'postgres'  # Временно используем стандартную БД
+    'password': '1234',  
+    'database': 'postgres'  
 }
 
 def create_database():
-    """Создание базы данных phonebook_db"""
     conn = psycopg2.connect(**DB_CONFIG)
     conn.autocommit = True
     cur = conn.cursor()
     
-    # Проверяем существует ли база данных
     cur.execute("SELECT 1 FROM pg_database WHERE datname = 'phonebook_db'")
     if not cur.fetchone():
         cur.execute("CREATE DATABASE phonebook_db")
@@ -27,14 +24,12 @@ def create_database():
     conn.close()
 
 def create_table():
-    """Создание таблицы contacts"""
     config = DB_CONFIG.copy()
     config['database'] = 'phonebook_db'
     
     conn = psycopg2.connect(**config)
     cur = conn.cursor()
     
-    # Создаем таблицу
     cur.execute("""
         CREATE TABLE IF NOT EXISTS contacts (
             id SERIAL PRIMARY KEY,
